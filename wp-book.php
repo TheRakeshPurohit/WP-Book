96eee2d550008ca63ccb7bb3ec0a8fad3fc8a033
therakeshpurohit <rakeshpurohit909@gmail.com>
2020-05-26 17:59:13 +0530

    custom meta box in progress

--- wp-book.php
+++ wp-book.php
@@ -56,8 +56,8 @@ function deactivate_wp_book() {
 
 if ( ! function_exists( 'wpbook_setup_post_type' ) ) {
 	/**
-	* Register the book custom post type.
-	*/
+	 * Register the book custom post type.
+	 */
 	function wpbook_setup_post_type() {
 		register_post_type(
 			'book',
@@ -158,6 +158,60 @@ function book_tags_nonhierarchical_taxonomy() {
 	);
 
 }
+/**
+ *
+ * Custom metabox book info
+ *
+ * @return void
+ */
+function wporg_add_custom_box() {
+	$screens = array( 'post', 'wporg_cpt' );
+	foreach ( $screens as $screen ) {
+			add_meta_box(
+				'wporg_box_id',           // Unique ID.
+				'Custom Meta Box Title',  // Box title.
+				'wporg_custom_box_html',  // Content callback, must be of type callable.
+				$screen                   // Post type.
+			);
+	}
+}
+add_action( 'add_meta_boxes', 'wporg_add_custom_box' );
+
+/**
+ * Custom meta box type.
+ *
+ * @param [type] $post
+ * @return void
+ */
+function wporg_custom_box_html( $post ) {
+	$value = get_post_meta( $post->ID, '_wporg_meta_key', true );
+	?>
+	<label for="wporg_field">Description for this field</label>
+	<select name="wporg_field" id="wporg_field" class="postbox">
+	<option value="">Select something...</option>
+	<option value="something" <?php selected( $value, 'something' ); ?>>Something</option>
+	<option value="else" <?php selected( $value, 'else' ); ?>>Else</option>
+	</select>
+	<?php
+}
+
+/**
+ * save post data
+ *
+ * @param [type] $post_id
+ * @return void
+ */
+function wporg_save_postdata( $post_id ) {
+	if ( array_key_exists( 'wporg_field', $_POST ) ) {
+		update_post_meta(
+			$post_id,
+			'_wporg_meta_key',
+			$_POST['wporg_field']
+		);
+	}
+}
+add_action( 'save_post', 'wporg_save_postdata' );
+
 
 register_activation_hook( __FILE__, 'activate_wp_book' );
 register_deactivation_hook( __FILE__, 'deactivate_wp_book' );
